43c95486b3619600c292c6710c10e6c705ead55a
im23b-terenzianie <terenzianie@bzz.ch>
2025-03-20 15:12:54 +0100

    Header versucht zu verbessern

--- components/header.js
+++ components/header.js
@@ -7,24 +7,25 @@ export default function Header() {
                 <h1 className="text-[#FF6B6B] font-bold text-xl">
                     Enzo Terenziani
                 </h1>
-                <nav className="grid">
-                    <ul className="flex items-center space-x-6 text-[#37424f] list-none">
+            </div>
+
+            <div className="flex items-center space-x-5 text-sm">
+                <nav>
+                    <ul className="flex gap-8 text-[#37424f] list-none">
                         <li>
-                            <Link href="/" className="text-[#FF6B6B]">About me</Link>
+                            <Link href="#about" className="text-[#37424f] hover:text-[#FF6B6B]">About me</Link>
                         </li>
                         <li>
-                            <Link href="/about" className="text-[#FF6B6B]">Project</Link>
+                            <Link href="#projects" className="text-[#37424f] hover:text-[#FF6B6B]">Projects</Link>
                         </li>
                         <li>
-                            <Link href="/contact" className="text-[#FF6B6B]">Contact</Link>
+                            <Link href="#contact" className="text-[#37424f] hover:text-[#FF6B6B]">Contact</Link>
                         </li>
                     </ul>
                 </nav>
-            </div>
-            <div className="flex items-center justify-evenly space-x-5 text-sm">
-        <span className="text-[#37424f] underline underline-offset-2 decoration-2 cursor-pointer">
-          DE
-        </span>
+                <span className="text-[#37424f] underline underline-offset-2 decoration-2 cursor-pointer">
+                    DE
+                </span>
                 <span className="text-[#c1c6cc] cursor-pointer">EN</span>
             </div>
         </div>
