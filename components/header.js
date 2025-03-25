e3e75e77a5500013cf49c1e66ba85abad8384841
im23b-terenzianie <terenzianie@bzz.ch>
2025-03-25 09:42:06 +0100

    Header versucht zu verbessern

--- components/header.js
+++ components/header.js
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <div className="flex items-center justify-between px-4 py-2">
+        <div className="grid grid-cols-2 items-center px-4 py-2">
             <div className="flex items-center space-x-8">
                 <h1 className="text-[#FF6B6B] font-bold text-xl">
                     Enzo Terenziani
                 </h1>
             </div>
 
-            <div className="flex items-center space-x-5 text-sm">
+            <div className="flex items-center justify-end space-x-5 text-sm">
                 <nav>
                     <ul className="flex gap-8 text-[#37424f] list-none">
                         <li>
