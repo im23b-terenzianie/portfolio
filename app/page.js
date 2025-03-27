db99158e4fca8f3e14f67c04283dca2d516f48af
im23b-terenzianie <terenzianie@bzz.ch>
2025-03-27 15:24:05 +0100

    Added a Picture

--- app/page.js
+++ app/page.js
@@ -3,9 +3,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="" id="home">
-      <h1>Enzo Terenziani</h1>
+        <div className="flex justify-between">
+            <h1>Enzo Terenziani</h1>
+            <Image
+                src="/IMG_0394.JPG"
+                alt="Enzo Terenziani"
+                width={300}
+                height={300}
+                className="rounded-full"
+            />
 
-      
+        </div>
+
+
     </div>
   );
 }
